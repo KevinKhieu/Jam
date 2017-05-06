004b016b0b09cbc35bc6b5f434444554d0e80d1a
Lucas <lucasx@stanford.edu>
2017-05-06 15:15:27 -0700

    introduced socket io backend

--- home/www/webServer.js
+++ home/www/webServer.js
@@ -7,6 +7,10 @@
  * 2. node webServer.js
  *
  * Application currently runs at localhost:3000.
+ *
+ * Note that anyone able to connect to localhost:3000 will be able to fetch any file accessible
+ * to the current user in the current directory or any of its children.
+ * TODO: fix this.
  */
 
 // For parsing request body parameters
@@ -31,27 +35,36 @@ var processFormBody = multer({storage: multer.memoryStorage()}).single('uploaded
 // Schema for Well Entries (Mongoose)
 var Entry = require('./schema/entry.js');
 
-// HTTP
-var http = require('http');
-var portno = 3000;   // Port number to use
-var fs = require('fs');
-
 // Express - client for easy communication between backend and frontend
 var express = require('express');
 var app = express();
 
-app.use(bodyParser.json());
+// Server Setup //
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('a user disconnected');
+  });
+  socket.on('upvote', function(sid) {
+    console.log('user upvoted ' + sid);
+  });
+});
+
+var portno = 3000;  // Port number to use
+http.listen(portno, function() {
+  console.log('Listening at http://localhost:' + portno + ' exporting the directory ' + __dirname);
+});
 
 // Sets working directory (directory loaded) to __dirname, which is "Jam/home/www"
 // Not sure where I set __dirname though...
 app.use(express.static(__dirname));
+var fs = require('fs');
 
-
-// Server startup
-var server = app.listen(portno, function () {
-  var port = server.address().port;
-  console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
-});
+app.use(bodyParser.json());
 
 // API ROUTES BELOW
 
