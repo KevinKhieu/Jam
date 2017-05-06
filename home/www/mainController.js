fe74a4ad4585fbbfde406aad29939cfc20ee946b
Kevin Khieu <kkhieu@stanford.edu>
2017-05-06 02:38:17 -0700

    Front end nearly done

--- home/www/mainController.js
+++ home/www/mainController.js
@@ -5,6 +5,24 @@ var jamApp = angular.module('jamApp', ['ngRoute', 'ngMaterial', 'ngResource']);
 jamApp.controller('MainController', ['$scope', '$rootScope', '$location', '$http', '$routeParams', '$resource', '$mdDialog', '$mdMedia',
     function ($scope, $rootScope, $location, $http, $routeParams, $resource, $mdDialog, $mdMedia) {
         
+    	$scope.main = {};
+    	$scope.main.songName = "SHAPE OF YOU";
+    	$scope.main.artist = "ED SHEERAN";
+    	$scope.main.lastPlayedArtist = "The Chainsmokers";
+    	$scope.main.lastPlayedTitle = "Paris"
+    	$scope.main.playlist = [{name: "Yellow", artist: "Coldplay"}, 
+    		{name: "Yellow", artist: "Coldplay"}, 
+    		{name: "Yellow", artist: "Coldplay"}, 
+    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Yellow", artist: "Coldplay"}];
 
         
 }]);      
