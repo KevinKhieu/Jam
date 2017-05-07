c0f29a8112a781b64a3b05d7022fee7b64f0e0fc
Kevin Khieu <kkhieu@stanford.edu>
2017-05-06 21:05:04 -0700

    safety - contains soundcloud stuff

--- home/www/mainController.js
+++ home/www/mainController.js
@@ -2,14 +2,48 @@
 
 var jamApp = angular.module('jamApp', ['ngRoute', 'ngMaterial', 'ngResource']);
 
+$(document).ready(function() {
+  // initialization
+  SC.initialize({
+    client_id: "15c5a12b5d640af73b16bd240753ffbb"
+  });
+
+  // Play audio
+  $("#embedTrack").click(function() {
+    var player = $("#player");
+    SC.oEmbed('https://soundcloud.com/mureed-abbas-shah/sami-meri-waar-by-qb-umair', {
+      maxheight: 200
+    }, function(res) {
+      $("#player").html(res.html);
+    });
+  });
+
+  // SC.get('/tracks', {
+  //   q: 'Boyce Avenue',
+  //   license: 'cc-by-sa'
+  //   }, function(tracks) {
+  //     console.log(tracks);
+  //   });
+
+    SC.stream('/tracks/293').then(function(player){
+      player.play();
+    });
+})
+
 jamApp.controller('MainController', ['$scope', '$rootScope', '$location', '$http', '$routeParams', '$resource', '$mdDialog', '$mdMedia',
     function ($scope, $rootScope, $location, $http, $routeParams, $resource, $mdDialog, $mdMedia) {
         
     	$scope.main = {};
+
+
     	$scope.main.songName = "SHAPE OF YOU";
     	$scope.main.artist = "ED SHEERAN";
+
+
     	$scope.main.lastPlayedArtist = "The Chainsmokers";
     	$scope.main.lastPlayedTitle = "Paris"
+
+
     	$scope.main.playlist = [{name: "Yellow", artist: "Coldplay"}, 
     		{name: "Fireflies", artist: "Owl City"}, 
     		{name: "Stained Glass", artist: "John Guerra"}, 
