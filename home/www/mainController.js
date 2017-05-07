b696059625f21ed382c33e7c498bc458f08c322a
Kevin Khieu <kkhieu@stanford.edu>
2017-05-06 20:34:26 -0700

    Front end favoriting and listing

--- home/www/mainController.js
+++ home/www/mainController.js
@@ -11,18 +11,23 @@ jamApp.controller('MainController', ['$scope', '$rootScope', '$location', '$http
     	$scope.main.lastPlayedArtist = "The Chainsmokers";
     	$scope.main.lastPlayedTitle = "Paris"
     	$scope.main.playlist = [{name: "Yellow", artist: "Coldplay"}, 
-    		{name: "Yellow", artist: "Coldplay"}, 
-    		{name: "Yellow", artist: "Coldplay"}, 
-    		{name: "Yellow", artist: "Coldplay"},
-    		{name: "Yellow", artist: "Coldplay"},
-    		{name: "Yellow", artist: "Coldplay"},
-    		{name: "Yellow", artist: "Coldplay"},
-    		{name: "Yellow", artist: "Coldplay"},
+    		{name: "Fireflies", artist: "Owl City"}, 
+    		{name: "Stained Glass", artist: "John Guerra"}, 
+    		{name: "Perfect", artist: "One Direction"},
+    		{name: "Firework", artist: "Katy Perry"},
+    		{name: "Where The Story Ends", artist: "The Fray"},
+    		{name: "You Found Me", artist: "The Fray"},
+    		{name: "Castle on the Hill", artist: "Ed Sheeran"},
     		{name: "Yellow", artist: "Coldplay"},
     		{name: "Yellow", artist: "Coldplay"},
     		{name: "Yellow", artist: "Coldplay"},
     		{name: "Yellow", artist: "Coldplay"},
     		{name: "Yellow", artist: "Coldplay"}];
+
+
+        $('a.like-button').on('click', function() {
+          $(this).toggleClass('liked');
+        });
 
         
 }]);      
