0deca56479a87e607592a06c2ee3e7f2e9b9cbe6
Lucas <lucasx@stanford.edu>
2017-05-09 16:22:08 -0700

    tabify and submit button

--- home/www/mainController.js
+++ home/www/mainController.js
@@ -1,67 +1,58 @@
 'use strict';
 
-var jamApp = angular.module('jamApp', ['ngRoute', 'ngMaterial', 'ngResource']);
-
+/* Soundcloud API initialization */
 $(document).ready(function() {
-  // initialization
-  SC.initialize({
-    client_id: "15c5a12b5d640af73b16bd240753ffbb"
-  });
+	// initialization
+	SC.initialize({
+		client_id: "15c5a12b5d640af73b16bd240753ffbb"
+	});
 
-  // Play audio
-  $("#embedTrack").click(function() {
-    var player = $("#player");
-    SC.oEmbed('https://soundcloud.com/mureed-abbas-shah/sami-meri-waar-by-qb-umair', {
-      maxheight: 200
-    }, function(res) {
-      $("#player").html(res.html);
-    });
-  });
+	// Play audio
+	$("#embedTrack").click(function() {
+		var player = $("#player");
+		SC.oEmbed('https://soundcloud.com/mureed-abbas-shah/sami-meri-waar-by-qb-umair', {
+			maxheight: 200
+		}, function(res) {
+			$("#player").html(res.html);
+		});
+	});
 
-  // SC.get('/tracks', {
-  //   q: 'Boyce Avenue',
-  //   license: 'cc-by-sa'
-  //   }, function(tracks) {
-  //     console.log(tracks);
-  //   });
+	// SC.get('/tracks', {
+	// 	q: 'Boyce Avenue',
+	// 	license: 'cc-by-sa'
+	// 	}, function(tracks) {
+	// 		console.log(tracks);
+	// 	});
 
-    SC.stream('/tracks/293').then(function(player){
-      player.play();
-    });
+	SC.stream('/tracks/293').then(function(player){
+		player.play();
+	});
 })
 
+var jamApp = angular.module('jamApp', ['ngRoute', 'ngMaterial', 'ngResource']);
+
 jamApp.controller('MainController', ['$scope', '$rootScope', '$location', '$http', '$routeParams', '$resource', '$mdDialog', '$mdMedia',
-    function ($scope, $rootScope, $location, $http, $routeParams, $resource, $mdDialog, $mdMedia) {
+	function ($scope, $rootScope, $location, $http, $routeParams, $resource, $mdDialog, $mdMedia) {
 
-    	$scope.main = {};
-
-
-    	$scope.main.songName = "SHAPE OF YOU";
-    	$scope.main.artist = "ED SHEERAN";
+		$scope.main = {};
 
 
-    	$scope.main.lastPlayedArtist = "The Chainsmokers";
-    	$scope.main.lastPlayedTitle = "Paris"
+		$scope.main.songName = "SHAPE OF YOU";
+		$scope.main.artist = "ED SHEERAN";
 
 
-    	$scope.main.playlist = [{name: "Yellow", artist: "Coldplay"},
-    		{name: "Fireflies", artist: "Owl City"},
-    		{name: "Stained Glass", artist: "John Guerra"},
-    		{name: "Perfect", artist: "One Direction"},
-    		{name: "Firework", artist: "Katy Perry"},
-    		{name: "Where The Story Ends", artist: "The Fray"},
-    		{name: "You Found Me", artist: "The Fray"},
-    		{name: "Castle on the Hill", artist: "Ed Sheeran"},
-    		{name: "Yellow", artist: "Coldplay"},
-    		{name: "Yellow", artist: "Coldplay"},
-    		{name: "Yellow", artist: "Coldplay"},
-    		{name: "Yellow", artist: "Coldplay"},
-    		{name: "Yellow", artist: "Coldplay"}];
+		$scope.main.lastPlayedArtist = "The Chainsmokers";
+		$scope.main.lastPlayedTitle = "Paris"
 
 
-        $('a.like-button').on('click', function() {
-          $(this).toggleClass('liked');
-        });
+		$scope.main.playlist = [
+			{name: "Yellow", artist: "Coldplay", spotifyId: 9}
+		];
+
+
+			$('a.like-button').on('click', function() {
+				$(this).toggleClass('liked');
+			});
 
 
 }]);
