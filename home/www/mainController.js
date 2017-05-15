9c1692d57a761cb3e7ae437f7f0494c2ae877860
Kevin Khieu <kkhieu@stanford.edu>
2017-05-14 20:51:38 -0700

    Safety commit

--- home/www/mainController.js
+++ home/www/mainController.js
@@ -4,30 +4,30 @@ var jamApp = angular.module('jamApp', ['ngRoute', 'ngMaterial', 'ngResource']);
 
 $(document).ready(function() {
   // initialization
-  SC.initialize({
-    client_id: "15c5a12b5d640af73b16bd240753ffbb"
-  });
+    SC.initialize({
+        client_id: "02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea"
+    });
 
   // Play audio
-  $("#embedTrack").click(function() {
+    $("#embedTrack").click(function() {
     var player = $("#player");
-    SC.oEmbed('https://soundcloud.com/mureed-abbas-shah/sami-meri-waar-by-qb-umair', {
-      maxheight: 200
-    }, function(res) {
-      $("#player").html(res.html);
+        SC.oEmbed('https://soundcloud.com/mureed-abbas-shah/sami-meri-waar-by-qb-umair', {
+            maxheight: 200
+        }, function(res) {
+            $("#player").html(res.html);
+        });
     });
-  });
-
-  // SC.get('/tracks', {
-  //   q: 'Boyce Avenue',
-  //   license: 'cc-by-sa'
-  //   }, function(tracks) {
-  //     console.log(tracks);
   //   });
 
     SC.stream('/tracks/293').then(function(player){
       player.play();
     });
+
+    // API Key: last.fm
+    // 254aca4d820bd8096466bcdc76783feb
+
+    // secret:
+    // b5a931498797ae2080f21bac4599842d
 })
 
 jamApp.controller('MainController', ['$scope', '$rootScope', '$location', '$http', '$routeParams', '$resource', '$mdDialog', '$mdMedia',
