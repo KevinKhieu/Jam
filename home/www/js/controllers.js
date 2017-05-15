6c9796a0744f615211fbefda4d70232d6ed6a1b5
Kevin Khieu <kkhieu@stanford.edu>
2017-05-15 01:18:30 -0700

    Mock for Arun

--- home/www/js/controllers.js
+++ home/www/js/controllers.js
@@ -9,6 +9,55 @@ angular.module('controller', ['songServices'])
 	function($scope, songs, socket, socket_controller) {
 
 		/* DEBUGGING CONSTANTS */
+		// 29kK_62LJV5bCprwtwIR1cKy
+		// var OAUTH2_CLIENT_ID = '19440559455-6fb0ug0s2usen2ruv8eg3phcrfv97322.apps.googleusercontent.com';
+		// var OAUTH2_SCOPES = [
+		//   'https://www.googleapis.com/auth/youtube'
+		// ];
+		// // googleApiClientReady = function() {
+		// //   gapi.auth.init(function() {
+		// //     window.setTimeout(checkAuth, 1);
+		// //   });
+		// // }
+
+		// function checkAuth() {
+		//   gapi.auth.authorize({
+		//     client_id: OAUTH2_CLIENT_ID,
+		//     scope: OAUTH2_SCOPES,
+		//     immediate: true
+		//   }, handleAuthResult);
+		// }
+
+		// // Handle the result of a gapi.auth.authorize() call.
+		// function handleAuthResult(authResult) {
+		//   if (authResult && !authResult.error) {
+		//     // Authorization was successful. Hide authorization prompts and show
+		//     // content that should be visible after authorization succeeds.
+		//     $('.pre-auth').hide();
+		//     $('.post-auth').show();
+		//     loadAPIClientInterfaces();
+		//   } else {
+		//     // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
+		//     // client flow. The current function is called when that flow completes.
+		//     $('#login-link').click(function() {
+		//       gapi.auth.authorize({
+		//         client_id: OAUTH2_CLIENT_ID,
+		//         scope: OAUTH2_SCOPES,
+		//         immediate: false
+		//         }, handleAuthResult);
+		//     });
+		//   }
+		// }
+
+		// // Load the client interfaces for the YouTube Analytics and Data APIs, which
+		// // are required to use the Google APIs JS client. More info is available at
+		// // https://developers.google.com/api-client-library/javascript/dev/dev_jscript#loading-the-client-library-and-the-api
+		// function loadAPIClientInterfaces() {
+		//   gapi.client.load('youtube', 'v3', function() {
+		//     handleAPILoaded();
+		//   });
+		// }
+
 
 		$scope.main = {};
 
@@ -34,6 +83,26 @@ angular.module('controller', ['songServices'])
 			} else {
 				console.log("UNLIKED")
 			}
+		}
+
+		function handleAPILoaded() {
+		  $('#search-button').attr('disabled', false);
+		}
+
+		// Search for a specified string.
+		$scope.search = function () {
+		  var q = $('#search_bar').val();
+		  console.log(gapi);
+		  var request = gapi.client.youtube.search.list({
+		    q: q,
+		    part: 'snippet'
+		  });
+
+
+		  request.execute(function(response) {
+		    var str = JSON.stringify(response.result);
+		    $('#search-container').html('<pre>' + str + '</pre>');
+		  });
 		}
 
 		/** Angular event handlers **/
@@ -65,6 +134,7 @@ angular.module('controller', ['songServices'])
 		$("#search_bar").on('keyup', function (e) {
 		    if (e.keyCode == 13) {
 		        $scope.addSong();
+		        // $scope.search();
 		    }
 		});
 		// RESET
